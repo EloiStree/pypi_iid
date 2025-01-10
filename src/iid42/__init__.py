d9996f1248d30ecd6d392299daad86f1ff7ee206
Eloi Strée <eloistree@gmail.com>
2025-01-10 22:52:38 +0100

    save push udp example fo rust listener

--- src/iid42/__init__.py
+++ src/iid42/__init__.py
@@ -707,12 +707,13 @@ if __name__ == "__main__":
     offset= NtpOffsetFetcher.get_global_ntp_offset_in_milliseconds()
     print (f"Default Global NTP Offset: {offset}")
 
-    bool_loop_listener_test = True
+    bool_loop_listener_test = False
     if bool_loop_listener_test:
         print("Loop Listener Test")
         target = ListenUdpIID("0.0.0.0",3615, offset)
       
-        
+    
+
 
     bool_console_test = False
     if bool_console_test : 
@@ -723,24 +724,41 @@ if __name__ == "__main__":
     bool_queue_test = False
     if bool_queue_test:
         print("Queue Test")
-        target = SendUdpIID("apint.ddns.net",3615,True,True)
+        server_name= "apint.ddns.net"
+        server_name= "127.0.0.1"
+        target = SendUdpIID(server_name,3615,True,True)
         print("IVP4", target.ivp4)
-        target.push_index_integer_in_queue(1,1082,0)
-        target.push_index_integer_in_queue(1,2082,1000)
-        target.push_index_integer_in_queue(1,1037,2000)
-        target.push_index_integer_in_queue(1,2037,4000)
-        target.push_index_integer_in_queue(1,1038,5000)
-        target.push_index_integer_in_queue(1,2038,6000)
-        target.push_index_integer_in_queue(1,1039,7000)
-        target.push_index_integer_in_queue(1,2039,8000)
-        
-        target.push_integer_in_queue(2082,11000)
-        target.push_integer_in_queue(1037,12000)
-        target.push_integer_in_queue(2037,14000)
-        target.push_integer_in_queue(1038,15000)
-        target.push_integer_in_queue(2038,16000)
-        target.push_integer_in_queue(1039,17000)
-        target.push_integer_in_queue(2039,18000)
+        while True:
+            target.push_index_integer_in_queue(1,1082,0)
+            target.push_index_integer_in_queue(1,2082,1000)
+            target.push_index_integer_in_queue(1,1037,2000)
+            target.push_index_integer_in_queue(1,2037,4000)
+            target.push_index_integer_in_queue(1,1038,5000)
+            target.push_index_integer_in_queue(1,2038,6000)
+            target.push_index_integer_in_queue(1,1039,7000)
+            target.push_index_integer_in_queue(1,2039,8000)
+            
+            target.push_integer_in_queue(2082,11000)
+            target.push_integer_in_queue(1037,12000)
+            target.push_integer_in_queue(2037,14000)
+            target.push_integer_in_queue(1038,15000)
+            target.push_integer_in_queue(2038,16000)
+            target.push_integer_in_queue(1039,17000)
+            target.push_integer_in_queue(2039,18000)
+            time.sleep(5)
+            
+    bool_queue_test_ntp=True
+    if bool_queue_test_ntp:
+        print("Queue Test NTP")
+        server_name= "apint.ddns.net"
+        server_name= "127.0.0.1"
+        target = SendUdpIID(server_name,3615,True,True)
+        while True:
+            target.push_index_integer_date_ntp_in_milliseconds(1,1259,5000)
+            for i in range(0, 10):
+                target.push_index_integer_date_ntp_in_milliseconds(1,i,0)
+                time.sleep(1)
+            
         
     
     while(True):
